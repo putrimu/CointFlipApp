d5a211fef17f769c5c17cd13681da2e7d2ef74bb
putrimu <putrimariahulfa@gmail.com>
2018-02-07 12:50:00 +0700

    Initial commit

---
+++ app/src/main/java/project/camp/bso/cointflipapp/CoinActivity.java
@@ -0,0 +1,13 @@
+package project.camp.bso.cointflipapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coin);
+    }
+}
